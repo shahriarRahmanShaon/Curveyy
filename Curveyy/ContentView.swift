f4a10ed3a831fffe721c08a40fe27685a340549e
shahriarRahmanShaon <shaon463@gmail.com>
2021-04-14 15:00:38 +0600

    Initial Commit

---
+++ Curveyy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Curveyy
+//
+//  Created by sergio shaon on 14/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Curveyy/CurveyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurveyyApp.swift
+//  Curveyy
+//
+//  Created by sergio shaon on 14/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CurveyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
